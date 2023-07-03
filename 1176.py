9ac085a382671b09bb14e76172e919c68e2e6634
marcoqavalett13 <104946375+marcoqavalett13@users.noreply.github.com>
2023-07-03 16:14:33 -0300

    Update 1176.py

--- 1176.py
+++ 1176.py
@@ -3,7 +3,7 @@ import time
 import matplotlib.pyplot as plt
 
 def minha_funcao(tempos):
-    # Registrar o tempo de início da execução da thread
+
     tempo_inicio = time.time()
 
 
@@ -20,33 +20,30 @@ def minha_funcao(tempos):
 
     print("O número de Fibonacci correspondente a", valor, "é", fib_n)
 
-    # Registrar o tempo de fim da execução da thread
     tempo_fim = time.time()
 
-    # Calcular a diferença de tempo
+
     tempo_total = tempo_fim - tempo_inicio
 
-    # Adicionar o tempo total à lista compartilhada
+
     tempos.append(tempo_total)
 
-# Obter a quantidade de threads a serem criadas
+
 quantidade_threads = int(input("Digite a quantidade de threads a serem criadas: "))
 
-# Lista para armazenar os tempos de execução (compartilhada entre as threads)
 tempos = []
 
-# Criar e iniciar as threads
+
 threads = []
 for _ in range(quantidade_threads):
     thread = threading.Thread(target=minha_funcao, args=(tempos,))
     thread.start()
     threads.append(thread)
 
-# Aguardar pela conclusão das threads
+
 for thread in threads:
     thread.join()
 
-# Gerar o gráfico
 plt.plot(range(1, len(tempos)+1), tempos)
 plt.xlabel('Thread')
 plt.ylabel('Tempo de execução')
