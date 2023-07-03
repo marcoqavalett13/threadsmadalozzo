1dbb1cce04be0cf2d0c61be0025660d105fa8987
marcoqavalett13 <104946375+marcoqavalett13@users.noreply.github.com>
2023-07-03 16:13:42 -0300

    Update 1101.py

--- 1101.py
+++ 1101.py
@@ -3,7 +3,6 @@ import time
 import matplotlib.pyplot as plt
 
 def minha_funcao(tempos):
-    # Registrar o tempo de início da execução da thread
     tempo_inicio = time.time()
 
     M = 100
@@ -17,34 +16,30 @@ def minha_funcao(tempos):
     soma = sum(sequencia)
     
 
-
-    # Registrar o tempo de fim da execução da thread
     tempo_fim = time.time()
 
-    # Calcular a diferença de tempo
     tempo_total = tempo_fim - tempo_inicio
 
-    # Adicionar o tempo total à lista compartilhada
+
     tempos.append(tempo_total)
 
-# Obter a quantidade de threads a serem criadas
+
 quantidade_threads = int(input("Digite a quantidade de threads a serem criadas: "))
 
-# Lista para armazenar os tempos de execução (compartilhada entre as threads)
+
 tempos = []
 
-# Criar e iniciar as threads
+
 threads = []
 for _ in range(quantidade_threads):
     thread = threading.Thread(target=minha_funcao, args=(tempos,))
     thread.start()
     threads.append(thread)
 
-# Aguardar pela conclusão das threads
+
 for thread in threads:
     thread.join()
 
-# Gerar o gráfico
 plt.plot(range(1, len(tempos)+1), tempos)
 plt.xlabel('Thread')
 plt.ylabel('Tempo de execução')
